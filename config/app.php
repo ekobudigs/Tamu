47a5e5665ab890dce3ff97d42c43ff263be0ce82
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-25 16:26:26 +0700

    Install Yajra Datatables

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         /*
          * Application Service Providers...
          */
+        Yajra\DataTables\DataTablesServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
