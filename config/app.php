c5e607f8b5cc5e395da092ec7a8f8a07dd5cbb5c
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-27 10:41:34 +0700

    add yajra datatables button config app

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
          * Application Service Providers...
          */
         Yajra\DataTables\DataTablesServiceProvider::class,
+        Yajra\DataTables\ButtonsServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
