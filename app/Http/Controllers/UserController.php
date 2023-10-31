d0332cc191a8f8fad6b24bb86093c89e42176126
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-31 16:00:03 +0700

    create function delete model users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\DataTables\UsersDataTable;
 
@@ -10,5 +11,16 @@ class UserController extends Controller
     public function index(UsersDataTable $dataTable)
     {
         return $dataTable->render('users.index');
+    }
+
+    public function destroy(User $user)
+    {
+        if ($user->delete()) {
+            return redirect('/users');
+        } else {
+            return response()->json([
+                'message' => 'Gagal menghapus user.',
+            ], 500);
+        }
     }
 }
