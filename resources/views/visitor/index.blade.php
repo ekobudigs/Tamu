572d599a144e9454f7bacc605ab041fab1f767e5
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-11-01 16:22:26 +0700

    create index blade tamu

---
+++ resources/views/visitor/index.blade.php
@@ -0,0 +1,27 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Users') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                        {!! $dataTable->table(['width' => '100%', 'class' => 'table table-striped table-bordered table-mini']) !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @push('scripts')
+        {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
+        <script>
+            $(document).ready(function() {
+
+            });
+        </script>
+    @endpush
+</x-app-layout>
