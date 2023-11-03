97c0102a02e1dcc27a62a988e1436e4b677b0b10
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-11-03 16:03:12 +0700

    Update Migration Department

---
+++ database/migrations/2023_11_03_090204_create_departments_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->id(); // Kolom id
+            $table->string('name'); // Kolom nama departemen
+            $table->text('description')->nullable(); // Kolom deskripsi (nullable)
+            $table->timestamps(); // Kolom created_at dan updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('departments');
+    }
+};
