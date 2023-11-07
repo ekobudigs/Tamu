28fb3a6790eb380daa4708c2081de339a945b27e
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-11-07 15:09:11 +0700

    update style create

--- resources/views/visitor/index.blade.php
+++ resources/views/visitor/index.blade.php
@@ -6,11 +6,17 @@
     </x-slot>
 
     <div class="py-12">
+        
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <button type="button"
-                    class="mt-5 ml-3 text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Create</button>
+                <div class="mt-6 ml-3">
+                    <a href="{{route('visitors.create')}}"
+                    class="mt-6 text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Create</a>
+                </div>
+             
                 <div class="p-6 text-gray-900">
+                    
                     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
                         {!! $dataTable->table(['width' => '100%', 'class' => 'table table-striped table-bordered table-mini']) !!}
                     </div>
