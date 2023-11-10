681f13ce104eb578929b4905ee169622cb062874
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-11-10 16:22:16 +0700

    Update Style button action visitors

--- resources/views/visitor/action.blade.php
+++ resources/views/visitor/action.blade.php
@@ -1,12 +1,14 @@
 <!-- Tombol Edit -->
-<a href="{{ route('visitors.edit', $row->id) }}"
-    class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-    Edit
-</a>
+<div class="  mt-2 mb-2">
+    <a href="{{ route('visitors.edit', $row->id) }}"
+        class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg px-1 py-1 text-sm text-center ">
+        Edit
+    </a>
+</div>
 
 <!-- Tombol Show -->
 <a href="{{ route('visitors.show', $row->id) }}"
-    class="text-white bg-gradient-to-r from-green-500 via-green-600 to-green-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+    class="text-white bg-gradient-to-r from-green-500 via-green-600 to-green-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-1 py-1 mt-2 text-center mr-2 ">
     Show
 </a>
 
@@ -16,7 +18,7 @@
     @method('DELETE')
 
     <button type="submit" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')"
-        class="text-white bg-gradient-to-r from-red-500 via-red-600 to-red-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+        class="text-white bg-gradient-to-r from-red-500 via-red-600 to-red-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-1 py-1 mt-2 text-center mr-2 mb-2">
         Delete
     </button>
 </form>
