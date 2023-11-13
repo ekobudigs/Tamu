9485c3209dbc92b786ba78177b201c5103c13f4d
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-11-13 11:29:23 +0700

    Update Blade Form Sesuai tabel Visitor Survey

--- resources/views/visitor_survey/create.blade.php
+++ resources/views/visitor_survey/create.blade.php
@@ -11,181 +11,43 @@
             @csrf
 
             <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+            <input type="hidden" name="visitor_id" value="{{ $visitor->id }}">
 
 
             <div class="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 <div class="w-full p-4">
-                    <div
-                        class="card mx-auto max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <div class="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 
-
-
-                        <div class="mb-6">
-                            <label for="name"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Kode Tamu') }}</label>
-                            <input id="no_visitors" type="text"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                name="no_visitors" value=" {{ $newKode }}" required readonly autofocus>
-
+                        <div class="w-full p-4">
+                            <div class="mb-6">
+                                <label for="survey_answer"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Survey Answer') }}</label>
+                                <textarea id="survey_answer"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    name="survey_answer" required autofocus></textarea>
+                            </div>
                         </div>
-
-                        <div class="mb-6">
-                            <label for="guest_name"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Nama Tamu') }}</label>
-                            <input id="guest_name" type="text"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('guest_name') is-invalid @enderror"
-                                name="guest_name" value="{{ old('guest_name') }}" required autocomplete="guest_name">
-                            @error('guest_name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
+                        <div class="w-full p-4">
+                            <div class="mb-6">
+                                <label for="survey_scale"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Survey Scale') }}</label>
+                                <div class="flex flex-wrap justify-between">
+                                    @for ($i = 1; $i <= 10; $i++)
+                                        <div class="w-1/5">
+                                            <label for="survey_scale_{{ $i }}"
+                                                class="block text-sm font-medium text-gray-700 dark:text-white">{{ $i }}</label>
+                                            <input id="survey_scale_{{ $i }}" type="radio"
+                                                name="survey_scale" value="{{ $i }}"
+                                                class="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300">
+                                        </div>
+                                    @endfor
+                                </div>
+                            </div>
                         </div>
-                        <div class="mb-6">
-                            <label for="phone_number"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('No Hp') }}</label>
-                            <input id="phone_number" type="text"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('phone_number') is-invalid @enderror"
-                                name="phone_number" value="{{ old('phone_number') }}" required
-                                autocomplete="phone_number">
-                            @error('phone_number')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                        <div class="mb-6">
-                            <label for="email"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Email') }}</label>
-                            <input id="email" type="text"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('email') is-invalid @enderror"
-                                name="email" value="{{ old('email') }}" required autocomplete="email">
-                            @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-
-                        <div class="mb-6">
-                            <label for="address"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Alamat') }}</label>
-                            <textarea id="address"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('address') is-invalid @enderror"
-                                name="address" required autocomplete="address">{{ old('address') }}</textarea>
-                            @error('address')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-
-
-
-
-
-
                     </div>
                 </div>
 
-                <div class="w-full p-4">
-                    <div
-                        class="card mx-auto max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 
-
-
-
-
-
-
-                        <div class="mb-6">
-                            <label for="type"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Type') }}</label>
-                            <select id="type"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('type') is-invalid @enderror"
-                                name="type" required autocomplete="type" autofocus>
-                                <option value="" @if (old('type') === null) selected @endif>Pilih Type
-                                </option>
-                                <option value="Guest" @if (old('type') === 'Guest') selected @endif>
-                                    {{ __('Pengunjung') }}</option>
-                                <option value="Visitor" @if (old('type') === 'Visitor') selected @endif>
-                                    {{ __('Tamu') }}</option>
-                            </select>
-                            @error('type')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-
-                        <div class="mb-6">
-                            <label for="department_id"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Departments') }}</label>
-                            <select id="department_id"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('department_id') is-invalid @enderror"
-                                name="department_id" required autocomplete="department_id" autofocus>
-                                <option value="" @if (old('department_id') === null) selected @endif>Pilih
-                                    Departemen</option>
-                                @foreach ($departments as $id => $name)
-                                    <option value="{{ $id }}"
-                                        @if (old('department_id') == $id) selected @endif>
-                                        {{ $name }}
-                                    </option>
-                                @endforeach
-                            </select>
-                            @error('department_id')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-
-
-                        <div class="mb-6">
-                            <label for="office_institution_name"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Nama Kantor') }}</label>
-                            <input id="office_institution_name" type="text"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('office_institution_name') is-invalid @enderror"
-                                name="office_institution_name" value="{{ old('office_institution_name') }}" required
-                                autocomplete="office_institution_name">
-                            @error('office_institution_name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-
-                        <div class="mb-6">
-                            <label for="number_of_people"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Jumlah Orang') }}</label>
-                            <input id="number_of_people" type="text"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('number_of_people') is-invalid @enderror"
-                                name="number_of_people" value="{{ old('number_of_people') }}" required
-                                autocomplete="number_of_people">
-                            @error('number_of_people')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-
-                        <div class="mb-6">
-                            <label for="purpose"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Tujuan/Alasan') }}</label>
-                            <textarea id="purpose"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('purpose') is-invalid @enderror"
-                                name="purpose" required autocomplete="purpose">{{ old('purpose') }}</textarea>
-                            @error('purpose')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-
-
-
-                    </div>
-                </div>
 
             </div>
             <div class="text-center mt-6 mb-6">
