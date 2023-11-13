8368e835716206a1a8c505b95e6ca7e5af414db3
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-11-13 11:47:12 +0700

    Update style Form survey scale

--- resources/views/visitor_survey/create.blade.php
+++ resources/views/visitor_survey/create.blade.php
@@ -14,41 +14,33 @@
             <input type="hidden" name="visitor_id" value="{{ $visitor->id }}">
 
 
-            <div class="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div class="mt-6 grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div class="w-full p-4">
-                    <div class="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-
-                        <div class="w-full p-4">
-                            <div class="mb-6">
-                                <label for="survey_answer"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Survey Answer') }}</label>
-                                <textarea id="survey_answer"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    name="survey_answer" required autofocus></textarea>
-                            </div>
-                        </div>
-                        <div class="w-full p-4">
-                            <div class="mb-6">
-                                <label for="survey_scale"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Survey Scale') }}</label>
-                                <div class="flex flex-wrap justify-between">
-                                    @for ($i = 1; $i <= 10; $i++)
-                                        <div class="w-1/5">
-                                            <label for="survey_scale_{{ $i }}"
-                                                class="block text-sm font-medium text-gray-700 dark:text-white">{{ $i }}</label>
-                                            <input id="survey_scale_{{ $i }}" type="radio"
-                                                name="survey_scale" value="{{ $i }}"
-                                                class="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300">
-                                        </div>
-                                    @endfor
+                    <div class="mb-6">
+                        <label for="survey_answer"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Survey Answer') }}</label>
+                        <textarea id="survey_answer"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            name="survey_answer" required autofocus></textarea>
+                    </div>
+                </div>
+                <div class="w-full p-4">
+                    <div class="mb-6">
+                        <label for="survey_scale"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">{{ __('Survey Scale') }}</label>
+                        <div class="flex flex-wrap justify-between">
+                            @for ($i = 1; $i <= 10; $i++)
+                                <div class="w-1/5">
+                                    <label for="survey_scale_{{ $i }}"
+                                        class="block text-sm font-medium text-gray-700 dark:text-black">{{ $i }}</label>
+                                    <input id="survey_scale_{{ $i }}" type="radio" name="survey_scale"
+                                        value="{{ $i }}"
+                                        class="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300">
                                 </div>
-                            </div>
+                            @endfor
                         </div>
                     </div>
                 </div>
-
-
-
             </div>
             <div class="text-center mt-6 mb-6">
                 <div class="col-md-6 offset-md-4">
