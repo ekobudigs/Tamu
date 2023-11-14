69f7d5e32a6e42e1956ee7c0415ef30edf8c73de
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-11-14 08:09:11 +0700

    Update Store Visitor Survey

--- app/Http/Controllers/VisitorSurveyController.php
+++ app/Http/Controllers/VisitorSurveyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Visitor;
 use Illuminate\Http\Request;
+use App\Models\VisitorSurvey;
 
 class VisitorSurveyController extends Controller
 {
@@ -14,5 +15,28 @@ class VisitorSurveyController extends Controller
         $visitor = Visitor::findOrFail($id);
         // return view visitor survey
         return view('visitor_survey.create', compact('visitor'));
+    }
+
+    // create function visitorsurvey store
+
+
+    public function store(Request $request)
+    {
+        // validate request
+        $request->validate([
+            'survey_answer' => 'required',
+            'survey_scale' => 'required',
+        ]);
+
+        // create visitor survey
+        $visitorSurvey = VisitorSurvey::create([
+            'user_id' => $request->user()->id,
+            'visitor_id' => $request->visitor_id,
+            'survey_answer' => $request->survey_answer,
+            'survey_scale' => $request->survey_scale,
+        ]);
+
+        // return redirect visitor survey
+        return redirect()->route('visitors.index')->with('success', 'Survey berhasil ditambahkan');
     }
 }
