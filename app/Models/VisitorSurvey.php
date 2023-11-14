2b5193a37347bc9c020b9f0e7581df091325030e
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-11-14 08:09:56 +0700

    Add Relation Visitor Survey

--- app/Models/VisitorSurvey.php
+++ app/Models/VisitorSurvey.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class VisitorSurvey extends Model
 {
     use HasFactory;
+    protected $fillable = ['user_id', 'visitor_id', 'survey_answer', 'survey_scale'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function visitor()
+    {
+        return $this->belongsTo(Visitor::class);
+    }
 }
