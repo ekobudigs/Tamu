29386800de28f098bfa84b7576a27e59ca501c64
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-11-17 15:54:45 +0700

    Update Fitur Scan Get Visit and Store update status

--- app/Http/Controllers/ScanController.php
+++ app/Http/Controllers/ScanController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Visitor;
 use Illuminate\Http\Request;
 
 class ScanController extends Controller
@@ -9,7 +10,31 @@ class ScanController extends Controller
 
     public function index()
     {
-        // tampilkan view scan
         return view('scan.index');
+    }
+
+
+    public function getVisitor(Request $request)
+    {
+        $visitor = Visitor::where('id', $request->id)->first();
+        if ($visitor) {
+            return redirect()->route('scan.index', ['visitor' => $visitor])->with('success', 'Visitor successfully scanned.');
+        } else {
+            return redirect()->back()->with('error', 'Visitor not found.');
+        }
+    }
+
+
+    public function store(Request $request)
+    {
+        $visitor = Visitor::where('id', $request->id)->first();
+        if ($visitor) {
+            $visitor->update([
+                'status' => '1'
+            ]);
+            return redirect()->back()->with('success', 'Visitor successfully scanned.');
+        } else {
+            return redirect()->back()->with('error', 'Visitor not found.');
+        }
     }
 }
