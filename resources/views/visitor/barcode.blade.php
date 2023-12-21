e8a61e7d489027f1e9e8425a32d414deb677a171
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-12-21 16:32:57 +0700

    Update h1

--- resources/views/visitor/barcode.blade.php
+++ resources/views/visitor/barcode.blade.php
@@ -4,11 +4,11 @@
             {{ __('Detail Tamu') }}
         </h2>
     </x-slot>
-
-    <div class="container">
-        <div
-            class="card mx-auto max-w-3xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="container mx-auto">
+        <div class="card max-w-3xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <h1 class="text-lg font-semibold dark:text-white">Tunjukan Kepada Satpam dan Repsionise</h1>
             {!! QrCode::size(256)->generate($visitor->no_visitors) !!}
         </div>
     </div>
+    
 </x-app-layout>
