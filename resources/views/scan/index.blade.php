c4854e6e6a116644018c87cd56eb04174c270299
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-12-27 12:19:38 +0700

    update add button validasi

--- resources/views/scan/index.blade.php
+++ resources/views/scan/index.blade.php
@@ -53,12 +53,9 @@
                                 </div>
                                 <!-- Modal footer -->
                                 <div
-                                    class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                                    <button data-modal-hide="default-modal" type="button"
-                                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
-                                        accept</button>
-                                    <button data-modal-hide="default-modal" type="button"
-                                        class="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+                                    class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600" id="validasi">
+                                   
+                                   
                                 </div>
                             </div>
                         </div>
@@ -92,6 +89,7 @@
                         console.log(response.visitor);
                         // Tambahkan logika atau manipulasi DOM lainnya di sini
                         displayVisitorInfo(response.visitor);
+                        displayButtonValidate();
                     } else {
                         // Pengunjung tidak ditemukan
                         console.log(response.message);
@@ -139,6 +137,22 @@ function displayNotFoundMessage() {
         <div>
             <p>Data tamu tidak ada di sistem. Silahkan laporkan ke admin.</p>
         </div>
+    `;
+
+    // Tampilkan pesan dalam div
+    infoDiv.innerHTML = html;
+}
+
+
+
+function displayButtonValidate() {
+    // Ambil elemen div untuk menampilkan pesan tidak ditemukan
+    const infoDiv = document.getElementById("validasi");
+
+    // Buat HTML untuk menampilkan pesan tidak ditemukan
+    const html = `
+    <button  type="button"
+                                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Validasi</button>
     `;
 
     // Tampilkan pesan dalam div
